09bc1ca2a439d750c8750cb903751aed6a2aee63
valentimhelio <valentimhelio@gmail.com>
2025-03-17 15:59:59 -0400

    Add project files.

---
+++ APICatalogo/Models/Produto.cs
@@ -0,0 +1,14 @@
+﻿namespace APICatalogo.Models
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string? Nome { get; set; } = string.Empty;
+        public string? Descricao { get; set; } = string.Empty;
+        public decimal Preco { get; set; }
+        public string? ImageUrl { get; set; }
+        public float Estoque { get; set; }
+        public DateTime DataCadastro { get; set; }
+
+    }
+}

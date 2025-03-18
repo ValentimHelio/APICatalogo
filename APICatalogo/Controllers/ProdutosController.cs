a1daa7adc2a0a90bc71549ab5601081f8298a317
valentimhelio <valentimhelio@gmail.com>
2025-03-18 15:36:32 -0400

    46. ProdutosController - DELETE

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -64,6 +64,21 @@ namespace APICatalogo.Controllers
             return Ok(produto);
         }
 
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            //var produto = _context.Produtos.Find(id);
+
+            if (produto is null)
+            {
+                return NotFound("Produto não localizado...");
+            }
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
 
     }
 }

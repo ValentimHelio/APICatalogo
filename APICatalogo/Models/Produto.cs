ac45eb559783c7d50c8f1cc1342457ac86aeab9d
valentimhelio <valentimhelio@gmail.com>
2025-03-18 10:19:03 -0400

    33. Apresentando o EF Core Migrations. 34. Aplicando o Migrations. 35. Apresentando o Data Annotations. 36. Aplicando o Data Annotations

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -1,11 +1,28 @@
-﻿namespace APICatalogo.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APICatalogo.Models
 {
+    [Table("Produtos")]
     public class Produto
     {
+        [Key]
         public int ProdutoId { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string? Nome { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(400)]
         public string? Descricao { get; set; } = string.Empty;
+
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
         public decimal Preco { get; set; }
+
+        [Required]
+        [StringLength(400)]
         public string? ImageUrl { get; set; }
         public float Estoque { get; set; }
         public DateTime DataCadastro { get; set; }

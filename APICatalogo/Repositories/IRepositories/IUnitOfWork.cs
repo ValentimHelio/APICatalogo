7e64c7cb0ae61845eb759929daf3abdfed3d888b
valentimhelio <valentimhelio@gmail.com>
2025-03-25 09:33:38 -0400

    89. Implementando o padrão Unit Of Work

---
+++ APICatalogo/Repositories/IRepositories/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace APICatalogo.Repositories.IRepositories
+{
+    public interface IUnitOfWork
+    {
+        IProdutoRepository produtoRepository { get; }
+        ICategoriaRepository categoriaRepository { get; }
+        void Commit();
+    }
+}

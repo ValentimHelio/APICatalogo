fee34a4600aa346b8e662a56d9caaa5336302105
valentimhelio <valentimhelio@gmail.com>
2025-04-07 15:15:47 -0400

    140. Autorização baseada em políticas com JWT. 141. Autorização baseada em políticas com JWT - criando um perfil (Role)

--- APICatalogo/Controllers/AuthController.cs
+++ APICatalogo/Controllers/AuthController.cs
@@ -15,16 +15,55 @@ public class AuthController : ControllerBase
 {
     private readonly ITokenService _tokenService;
     private readonly UserManager<ApplicationUser> _userManager;
-    private readonly RoleManager<IdentityRole> _roleManage;
+    private readonly RoleManager<IdentityRole> _roleManager;
     private readonly IConfiguration _configuration;
+    private readonly ILogger<AuthController> _logger;
 
-    public AuthController(ITokenService tokenService, UserManager<ApplicationUser> userManager,
-                                RoleManager<IdentityRole> roleManage, IConfiguration configuration)
+    public AuthController(ITokenService tokenService, UserManager<ApplicationUser> userManager, 
+                            RoleManager<IdentityRole> roleManage, IConfiguration configuration, ILogger<AuthController> logger)
     {
         _tokenService = tokenService;
         _userManager = userManager;
-        _roleManage = roleManage;
+        _roleManager = roleManage;
         _configuration = configuration;
+        _logger = logger;
+    }
+
+    [HttpPost]
+    [Route("CreateRole")]
+    public async Task<IActionResult> CreateRole(string roleName)
+    {
+        var roleExist = await _roleManager.RoleExistsAsync(roleName);
+
+        if (!roleExist)
+        {
+            var roleResult = await _roleManager.CreateAsync(new IdentityRole(roleName));
+
+            if (roleResult.Succeeded)
+            {
+                _logger.LogInformation(1, "Roles Added");
+                return StatusCode(StatusCodes.Status200OK,
+                        new Response
+                        {
+                            Status = "Success",
+                            Message =
+                        $"Role {roleName} added successfully"
+                        });
+            }
+            else
+            {
+                _logger.LogInformation(2, "Error");
+                return StatusCode(StatusCodes.Status400BadRequest,
+                   new Response
+                   {
+                       Status = "Error",
+                       Message =
+                       $"Issue adding the new {roleName} role"
+                   });
+            }
+        }
+        return StatusCode(StatusCodes.Status400BadRequest,
+          new Response { Status = "Error", Message = "Role already exist." });
     }
 
     [HttpPost]

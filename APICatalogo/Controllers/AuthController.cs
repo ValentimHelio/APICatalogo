3455e1e3935dfa746326b0478bcf620722dc106e
valentimhelio <valentimhelio@gmail.com>
2025-04-07 14:26:20 -0400

    135. AuthController - Register

--- APICatalogo/Controllers/AuthController.cs
+++ APICatalogo/Controllers/AuthController.cs
@@ -71,4 +71,35 @@ public class AuthController : ControllerBase
         return Unauthorized();
 
     }
+
+    [HttpPost]
+    [Route("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterModel model)
+    {
+        var userExists = await _userManager.FindByNameAsync(model.Username!);
+
+        if (userExists != null)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                   new Response { Status = "Error", Message = "User already exists!" });
+        }
+
+        ApplicationUser user = new()
+        {
+            Email = model.Email,
+            SecurityStamp = Guid.NewGuid().ToString(),
+            UserName = model.Username
+        };
+
+        var result = await _userManager.CreateAsync(user, model.Password!);
+
+        if (!result.Succeeded)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                   new Response { Status = "Error", Message = "User creation failed." });
+        }
+
+        return Ok(new Response { Status = "Success", Message = "User created successfully!" });
+
+    }
 }

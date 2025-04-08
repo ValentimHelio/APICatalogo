ca95f68979afa8851d689161e2a8150b5e3bc674
valentimhelio <valentimhelio@gmail.com>
2025-04-08 15:30:31 -0400

    146. Autenticação usando o manipulador Bearer Token

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -12,7 +12,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using System.Security.Claims;
 using System.Text;
 using System.Text.Json.Serialization;
 

9f246c67fa791fab11e42e73112a5ee0b65af40b
valentimhelio <valentimhelio@gmail.com>
2025-08-19 10:58:12 -0400

    168. Versionamento da API : Atributo MapToVersion

---
+++ APICatalogo/Controllers/TesteV3Controller.cs
@@ -0,0 +1,27 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+    [Route("api/teste")]
+    [ApiController]
+    [ApiVersion(3)]
+    [ApiVersion(4)]
+    public class TesteV3Controller : ControllerBase
+    {
+        [MapToApiVersion(3)]
+        [HttpGet]
+        public string GetVersion3()
+        {
+            return "Version3 - GET - API Versão 3.0"; //https://localhost:44351/api/teste?api-version=3
+        }
+
+        [MapToApiVersion(4)]
+        [HttpGet]
+        public string GetVersion4()
+        {
+            return "Version4 - GET - API Versão 4.0"; //https://localhost:44351/api/teste?api-version=4
+        }
+    }
+}
